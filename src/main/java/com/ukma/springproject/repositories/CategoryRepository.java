570dfd5a56926337cb0465fa9ed93234d8ffb7a7
Anastasia Zolotar <anazzlotar@gmail.com>
2022-10-25 14:17:13 +0300

    Repository classes added

---
+++ src/main/java/com/ukma/springproject/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.ukma.springproject.repositories;
+
+import com.ukma.springproject.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+}
---
+++ src/main/java/com/ukma/springproject/repositories/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.ukma.springproject.repositories;
+
+import com.ukma.springproject.domain.Category;
+import com.ukma.springproject.domain.Product;
+import com.ukma.springproject.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
+
+    List<Product> findAllByApplicationCategory(Category category);
+    List<Product> findAllByApplicationDeveloper(User user);
+
+}
