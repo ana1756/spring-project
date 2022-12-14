fac3c03f5218c04281685ce4ddd2970bcc4282ef
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2022-12-14 02:00:33 +0200

    add new endpoints to SecurityConfig

--- src/main/java/com/ukma/springproject/config/SecurityConfig.java
+++ src/main/java/com/ukma/springproject/config/SecurityConfig.java
@@ -15,9 +15,12 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .regexMatchers("/profile", "/getImage", "/profile/*").authenticated()
-                .regexMatchers("/applications*", "/admin/*").hasAuthority("ROLE_ADMIN")
-                .regexMatchers("/applications/create*").hasAuthority("ROLE_DEV")
+                .antMatchers("/profile", "/getImage", "/profile/**").authenticated()
+                .antMatchers("/rest/application/all", "/rest/application/developer/**",
+                        "/rest/application/{spring:\\d+}").hasAuthority("ROLE_ADMIN")
+                .antMatchers("/applications", "/admin/").hasAuthority("ROLE_ADMIN")
+                .antMatchers("/rest/application/create").hasAuthority("ROLE_DEV")
+                .regexMatchers("/applications/create").hasAuthority("ROLE_DEV")
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
